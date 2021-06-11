fae1834715c3c0a8993cabb8584f6f03f5d19692
CriptoGloumy <65531164+CriptoGloumy@users.noreply.github.com>
2021-06-11 13:51:45 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+					<div class="col-2">	
+						<img src="img/works/1.png" style="height: 100px" alt="1">
+					</div>
+					<div class="col-2">	
+						<img src="img/works/2.jpg" style="height: 100px" alt="2">
+					</div>
+					<div class="col-2">	
+						<img src="img/works/3.jpg" style="height: 100px" alt="3">
+					</div>	
